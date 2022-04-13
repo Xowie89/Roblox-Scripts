24150c7c8291e82a0c7a77b33c7a4f487716632a
Xowie89 <41758045+Xowie89@users.noreply.github.com>
2022-04-13 15:00:18 -0400

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,13 @@
+-- Anime Training Simulator
+if game.GameId == 7114796110 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Xowie89/Roblox-Scripts/main/Anime%20Training%20Simulator.lua"))()
+-- Hospital Tycoon
+elseif game.GameId == 7050008107 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Xowie89/Roblox-Scripts/main/Hospital%20Tycoon.lua"))()
+-- Factory Simulator
+elseif game.GameId == 6769764667 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Xowie89/Roblox-Scripts/main/Factory%20Simulator%20%5BOVERHAUL%5D.lua"))()
+-- Mall Tycoon
+elseif game.GameId == 5736409216 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Xowie89/Roblox-Scripts/main/Mall%20Tycoon.lua"))()
+end
