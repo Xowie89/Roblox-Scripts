911b0566f0eb52f09f05e31bbe71e47622f60b08
Xowie89 <41758045+Xowie89@users.noreply.github.com>
2022-10-04 23:57:26 -0400

    Update CommandUI.lua

--- CommandUI.lua
+++ CommandUI.lua
@@ -46,6 +46,25 @@ local viewing = nil
 
 local origsettings = {abt = Lighting.Ambient, oabt = Lighting.OutdoorAmbient, brt = Lighting.Brightness, time = Lighting.ClockTime, fe = Lighting.FogEnd, fs = Lighting.FogStart, gs = Lighting.GlobalShadows}
 
+--// Anti AFK \\--
+
+local GC = getconnections or get_signal_cons
+if GC then
+	for i,v in pairs(GC(Me.Idled)) do
+		if v["Disable"] then
+			v["Disable"](v)
+		elseif v["Disconnect"] then
+			v["Disconnect"](v)
+		end
+	end
+else
+	Me.Idled:Connect(function()
+		local VirtualUser = game:GetService("VirtualUser")
+		VirtualUser:CaptureController()
+		VirtualUser:ClickButton2(Vector2.new())
+	end)
+end
+
 --// Settings Save/Load \\--
 
 getgenv().settings = {}
